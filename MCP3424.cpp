263c26c9c7fc99620078b6cbc4977b958ed88ca5
Bernhard Schneider <bernhard@neaptide.org>
2014-10-22 19:29:17 +0200

    Update MCP3424.cpp
    minor

--- MCP3424.cpp
+++ MCP3424.cpp
@@ -3,7 +3,7 @@
  *   
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
- *  version 2.1 as published by the Free Software Foundation.
+ *  version 3 as published by the Free Software Foundation.
  * 
  */
 
