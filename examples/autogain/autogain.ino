5e7b1ddc1a19dfdceedc51fd5d2bc9481e91c752
bersch <bernhard@neaptide.org>
2014-10-23 17:52:33 +0200

    added examples/autogain/autogain.ino

---
+++ examples/autogain/autogain.ino
@@ -0,0 +1,48 @@
+#include <Wire.h>
+#include <MCP3424.h>
+
+MCP3424 adc(PIN_LOW, PIN_LOW);
+
+void setup() {
+
+    Serial.begin(57600);
+
+    Wire.begin();
+
+    adc.generalCall(GC_RESET);
+}
+
+void show_v(double& value, _ConfReg r) {
+
+    char v[24], buff[48];
+
+    dtostrf(value,8,8,v);
+    sprintf(buff, "channel: %d, gain: %d, value: %s", r.ch, 1 << r.pga, v);
+    Serial.println(buff);
+}
+
+double value;
+
+void loop() {
+
+    for (int i = (int)CH1; i <= (int)CH4; i++) {
+        _ConfReg& c = adc.creg[(Channel)i];
+        c.bits = { GAINx1, SR12B, ONE_SHOT, (Channel)i, 1 };
+        ConvStatus err = adc.read(c.ch, value);
+        if (err == R_OK) {
+            show_v(value, c);
+            c.srate = SR18B;
+            c.pga = adc.findGain(value);
+            err = adc.read(c.ch, value);
+            if (err == R_OK) {
+                show_v(value, c);
+                Serial.println();
+            } else 
+              Serial.println(" error.");
+        } else {
+            Serial.print("conversion error: ");
+            Serial.println(err);
+        }
+    }
+    asm volatile ("nop");
+}
